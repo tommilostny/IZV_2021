f24f385601f1cab527441d813a410ca0a85c438e
tommilostny <tommilostny@live.com>
2021-10-22 10:51:39 +0200

    Initial commit

---
+++ download.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import numpy as np
+import zipfile
+
+# Kromě vestavěných knihoven (os, sys, re, requests …) byste si měli vystačit s: gzip, pickle, csv, zipfile, numpy, matplotlib, BeautifulSoup.
+# Další knihovny je možné použít po schválení opravujícím (např ve fóru WIS).
+
+
+class DataDownloader:
+    """ TODO: dokumentacni retezce 
+
+    Attributes:
+        headers    Nazvy hlavicek jednotlivych CSV souboru, tyto nazvy nemente!  
+        regions     Dictionary s nazvy kraju : nazev csv souboru
+    """
+
+    headers = ["p1", "p36", "p37", "p2a", "weekday(p2a)", "p2b", "p6", "p7", "p8", "p9", "p10", "p11", "p12", "p13a",
+               "p13b", "p13c", "p14", "p15", "p16", "p17", "p18", "p19", "p20", "p21", "p22", "p23", "p24", "p27", "p28",
+               "p34", "p35", "p39", "p44", "p45a", "p47", "p48a", "p49", "p50a", "p50b", "p51", "p52", "p53", "p55a",
+               "p57", "p58", "a", "b", "d", "e", "f", "g", "h", "i", "j", "k", "l", "n", "o", "p", "q", "r", "s", "t", "p5a"]
+
+    regions = {
+        "PHA": "00",
+        "STC": "01",
+        "JHC": "02",
+        "PLK": "03",
+        "ULK": "04",
+        "HKK": "05",
+        "JHM": "06",
+        "MSK": "07",
+        "OLK": "14",
+        "ZLK": "15",
+        "VYS": "16",
+        "PAK": "17",
+        "LBK": "18",
+        "KVK": "19",
+    }
+
+    def __init__(self, url="https://ehw.fit.vutbr.cz/izv/", folder="data", cache_filename="data_{}.pkl.gz"):
+        pass
+
+    def download_data(self):
+        pass
+
+    def parse_region_data(self, region):
+        pass
+
+    def get_dict(self, regions=None):
+        pass
+
+
+# TODO vypsat zakladni informace pri spusteni python3 download.py (ne pri importu modulu)
---
+++ get_stat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+# povolene jsou pouze zakladni knihovny (os, sys) a knihovny numpy, matplotlib a argparse
+
+from download import DataDownloader
+
+
+def plot_stat(data_source,
+              fig_location=None,
+              show_figure=False):
+    pass
+
+
+# TODO pri spusteni zpracovat argumenty
