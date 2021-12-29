66b6f3246385de8ecd2732c6bd03cca27aacb074
tommilostny <tommilostny@live.com>
2021-12-29 22:44:40 +0100

    added make_geo first.

--- izv-part03-var-nehody/geo.py
+++ izv-part03-var-nehody/geo.py
@@ -1,31 +1,42 @@
 #!/usr/bin/python3.8
 # coding=utf-8
+# %%
 import pandas as pd
-import geopandas
+import geopandas as gpd
 import matplotlib.pyplot as plt
 import contextily
 import sklearn.cluster
 import numpy as np
 # muzete pridat vlastni knihovny
 
+# %%
+def make_geo(df: pd.DataFrame) -> gpd.GeoDataFrame:
+    """ Konvertovani dataframe do geopandas.GeoDataFrame se spravnym kodovanim. """
+    df = df.dropna(subset=['d', 'e']).copy()
 
-def make_geo(df: pd.DataFrame) -> geopandas.GeoDataFrame:
-    """ Konvertovani dataframe do geopandas.GeoDataFrame se spravnym kodovani"""
-    pass
+    return gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(x=df["d"], y=df["e"]), crs="epsg:3857")
 
-def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
+# %%
+def plot_geo(gdf: gpd.GeoDataFrame, fig_location: str = None,
              show_figure: bool = False):
     """ Vykresleni grafu s sesti podgrafy podle lokality nehody 
      (dalnice vs prvni trida) pro roky 2018-2020 """
     pass
 
-def plot_cluster(gdf: geopandas.GeoDataFrame, fig_location: str = None,
+# %%
+def plot_cluster(gdf: gpd.GeoDataFrame, fig_location: str = None,
                  show_figure: bool = False):
     """ Vykresleni grafu s lokalitou vsech nehod v kraji shlukovanych do clusteru """
     pass
 
+# %%
 if __name__ == "__main__":
+# %%
     # zde muzete delat libovolne modifikace
-    gdf = make_geo(pd.read_pickle("accidents.pkl.gz"))
+    df = pd.read_pickle("accidents.pkl.gz")
+# %%
+    gdf = make_geo(df)
+# %%
     plot_geo(gdf, "geo1.png", True)
+# %%
     plot_cluster(gdf, "geo2.png", True)
